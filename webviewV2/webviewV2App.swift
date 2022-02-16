1c6f38e68920f6f47741209f5e0a0d83dff55b12
matthew_lee <matthew_lee@gccompany.co.kr>
2022-02-16 17:34:42 +0900

    Initial Commit

---
+++ webviewV2/webviewV2App.swift
@@ -0,0 +1,17 @@
+//
+//  webviewV2App.swift
+//  webviewV2
+//
+//  Created by Matthew on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct webviewV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
