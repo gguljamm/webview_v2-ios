d29dbbbded3539ec4814f0b0881ddd0e10a5a26b
matthew_lee <matthew_lee@gccompany.co.kr>
2022-02-22 18:01:40 +0900

    ios webview init

--- webviewV2/ContentView.swift
+++ webviewV2/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var viewModel = WebViewModel()
+    @State private var username: String = "http://192.168.203.13:8080"
+    @State var status = false
+    
     var body: some View {
-        Text("Hello, world!")
+        if !status {
+            Text("WKWebView 테스터..").padding()
+            TextField(
+                "url (ex. http://192.168.203.13:8080)",
+                text: $username
+            )
+            .onSubmit {
+                self.status = true
+            }
+            .textInputAutocapitalization(.never)
+            .disableAutocorrection(true)
             .padding()
+            Button("열기") {
+                self.status = true
+            }
+        }
+        else {
+            WebView(url: username, viewModel: viewModel)
+        }
     }
 }
 
--- webviewV2/webviewV2App.swift
+++ webviewV2/webviewV2App.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct webviewV2App: App {
+    @State var status = false
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
